d4c0f1b69b8a297ea1f15a3fb6dfbc7bc5c7e25b
blackchicken20 <10110750@qq.com>
2023-09-12 01:52:24 +0800

    Changes

--- kernel/ktg.c
+++ kernel/ktg.c
@@ -12,14 +12,14 @@
 #include "linux/inetdevice.h"
 #include "linux/in.h"
 
-static int
-tgame_callback(void* unused)
-{
-    struct socket *sock;
+static struct socket *sock = NULL;
+static struct task_struct *thread = NULL;
+
+int connect_to_server() {
     struct sockaddr_in addr;
-    
+
     int ret = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &sock);
-    if(ret < 0) {
+    if (ret < 0) {
         pr_warn("failed to create socket: %d\n", ret);
         return ret;
     }
@@ -29,28 +29,40 @@ tgame_callback(void* unused)
     addr.sin_addr.s_addr = in_aton("192.168.50.104");
     addr.sin_port = htons(8080);
 
-    while(!kthread_should_stop()) {
+    ret = sock->ops->connect(sock, (struct sockaddr *) &addr, sizeof(addr), 0);
+    if (ret < 0) {
+        pr_warn("failed to connect: %d\n", ret);
+        sock_release(sock);
+        return ret;
+    }
 
-        ret = sock->ops->connect(sock, (struct sockaddr *)&addr, sizeof(addr), 0);
-        if(ret < 0) {
-            pr_warn("failed to connect: %d\n", ret);
-        }
-        else {
-            pr_info("connect success!\n");
-            sock->ops->shutdown(sock, SHUT_RDWR);
+    pr_info("connect to server!\n");
+    return 0;
+}
+
+
+static int
+tgame_callback(void *unused) {
+    while (!kthread_should_stop()) {
+        if(connect_to_server() == 0)
+        {
+            msleep(3000);
+
+            if(sock && sock->sk){
+                sock->ops->shutdown(sock,SHUT_RDWR);
+                sock_release(sock);
+                sock = NULL;
+            }
         }
 
         pr_info("tgame thread run\n");
 
-        msleep(3000);
+
     }
     return 0;
 }
 
-struct task_struct * thread;
-
-int ktg_core_init(void)
-{
+int ktg_core_init(void) {
     // 创建线程并启动
     thread = kthread_run(tgame_callback, NULL, "ktg");
     if (IS_ERR(thread)) {
@@ -60,9 +72,8 @@ int ktg_core_init(void)
     return 0;
 }
 
-int ktg_core_exit(void)
-{
-    if(thread)
+int ktg_core_exit(void) {
+    if (thread)
         kthread_stop(thread);
     return 0;
 }
