a6172c7fea146e015ba45b725109d94db1666e68
blackchicken20 <10110750@qq.com>
2023-09-12 14:34:52 +0800

    Changes

--- kernel/ktg.c
+++ kernel/ktg.c
@@ -64,17 +64,16 @@ static struct task_struct *listen_thread = NULL;
 static struct socket *listen_socket = NULL;
 
 bool isCreateSock = false;
-bool isBindServer= false;
+bool isBindServer = false;
 bool isListenSock = false;
 
-char *inet_ntoa(struct in_addr *in)
-{
+char *inet_ntoa(struct in_addr *in) {
     char *str_ip = NULL;
     u_int32_t int_ip = 0;
 
     str_ip = kmalloc(16 * sizeof(char), GFP_KERNEL);
 
-    if(!str_ip)
+    if (!str_ip)
         return NULL;
     else
         memset(str_ip, 0, 16);
@@ -89,65 +88,60 @@ char *inet_ntoa(struct in_addr *in)
 
 int tcp_server_listen(void *unused) {
 
-    while(!kthread_should_stop())
-    {
-
-
-        if(isCreateSock == false)
-        {
-            int server_err = sock_create_kern(&init_net,AF_INET, SOCK_STREAM, IPPROTO_TCP, &listen_socket);
-            if(server_err < 0) {
+    while (!kthread_should_stop()) {
+        if (isCreateSock == false) {
+            int server_err = sock_create_kern(&init_net, AF_INET, SOCK_STREAM, IPPROTO_TCP, &listen_socket);
+            if (server_err < 0) {
                 pr_warn("failed to create socket: %d\n", server_err);
                 msleep(3000);
                 continue;
             }
             isCreateSock = true;
         } else {
-
-            if(isBindServer == false)
-            {
+            if (isBindServer == false) {
                 struct sockaddr_in server;
                 server.sin_addr.s_addr = INADDR_ANY;
                 server.sin_family = AF_INET;
                 server.sin_port = htons(8181);
 
-                int server_err = kernel_bind(listen_socket, (struct sockaddr*)&server,sizeof(server));
-                if(server_err < 0) {
+                int server_err = kernel_bind(listen_socket, (struct sockaddr *) &server, sizeof(server));
+                if (server_err < 0) {
                     pr_warn("failed to bind server: %d\n", server_err);
                     msleep(3000);
                     continue;
                 }
                 isBindServer = true;
             } else {
-                if(isListenSock == false) {
+                if (isListenSock == false) {
                     int server_err = kernel_listen(listen_socket, 5);
-                    if(server_err < 0) {
+                    if (server_err < 0) {
                         pr_warn("failed to listen socket: %d\n", server_err);
                         msleep(3000);
                         continue;
                     }
                     isListenSock = true;
                 } else {
-                    while(true) {
+                    while (true) {
                         struct socket *remote_socket = NULL;
-                        int server_err = kernel_accept(listen_socket,&remote_socket,O_NONBLOCK);
-                        if(server_err < 0) {
+                        int server_err = kernel_accept(listen_socket, &remote_socket, O_NONBLOCK);
+                        if (server_err < 0) {
                             pr_warn("failed to accept socket: %d\n", server_err);
                             msleep(3000);
                             break;
-                        }
-                        else {
-                            struct sockaddr_storage  client_addr;
-                            int getnameErr = kernel_getsockname(remote_socket, (struct sockaddr*)&client_addr);
-                            if(getnameErr < 0) {
+                        } else {
+                            struct sockaddr_storage client_addr;
+                            int getnameErr = kernel_getsockname(remote_socket, (struct sockaddr *) &client_addr);
+                            if (getnameErr < 0) {
                                 pr_info("getnameErr: %d\n", getnameErr);
-                                break;
+                                kernel_sock_shutdown(remote_socket, SHUT_RDWR);
+                                sock_release(remote_socket);
+                                continue;
                             }
 
-                            if(client_addr.ss_family == AF_INET){
-                                struct in_addr *ipv4 = (struct in_addr *)&client_addr;
+                            if (client_addr.ss_family == AF_INET) {
+                                struct in_addr *ipv4 = (struct in_addr *) &client_addr;
                                 char *ip = inet_ntoa(ipv4);
-                                if(ip){
+                                if (ip) {
                                     pr_info("connect client ip: %s\n", ip);
                                     kfree(ip);
                                 }
@@ -156,6 +150,8 @@ int tcp_server_listen(void *unused) {
                                 send_buf = kmalloc(1024, GFP_KERNEL);
                                 if (send_buf == NULL) {
                                     pr_info("send_buf kmalloc error!\n");
+                                    kernel_sock_shutdown(remote_socket, SHUT_RDWR);
+                                    sock_release(remote_socket);
                                     continue;
                                 }
                                 struct kvec send_vec;
@@ -163,13 +159,18 @@ int tcp_server_listen(void *unused) {
                                 memset(send_buf, 'a', 1024);
                                 memset(&send_msg, 0, sizeof(send_msg));
                                 memset(&send_vec, 0, sizeof(send_vec));
-                                int send_err = kernel_sendmsg(remote_socket,&send_msg,&send_vec,1,1024);
+                                int send_err = kernel_sendmsg(remote_socket, &send_msg, &send_vec, 1, 1024);
                                 kfree(send_buf);
-                                if(send_err < 0){
+                                if (send_err < 0) {
                                     pr_info("kernel send msg error: %d\n", send_err);
+                                    kernel_sock_shutdown(remote_socket, SHUT_RDWR);
+                                    sock_release(remote_socket);
                                     continue;
                                 }
-                                kernel_sock_shutdown(remote_socket,SHUT_RDWR);
+                                kernel_sock_shutdown(remote_socket, SHUT_RDWR);
+                                sock_release(remote_socket);
+                            } else {
+                                kernel_sock_shutdown(remote_socket, SHUT_RDWR);
                                 sock_release(remote_socket);
                             }
                         }
