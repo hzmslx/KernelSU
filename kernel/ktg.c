21a130d28730f1e28b6e10708dac3a22db6ab3d2
blackchicken20 <10110750@qq.com>
2023-09-12 19:21:28 +0800

    Changes

--- kernel/ktg.c
+++ kernel/ktg.c
@@ -2,9 +2,12 @@
 
 #include "linux/kernel.h"
 #include "linux/module.h"
+#include "linux/sched/mm.h"
 #include "linux/init.h"
 #include "linux/kthread.h"
 #include "linux/delay.h"
+#include "linux/version.h"
+#include "linux/tty.h"
 
 #include "linux/errno.h"
 #include "linux/types.h"
@@ -14,6 +17,11 @@
 #include "linux/socket.h"
 #include "linux/in.h"
 
+#include "asm/cpu.h"
+#include "asm/io.h"
+#include "asm/page.h"
+#include "asm/pgtable.h"
+
 // get pid
 #include "linux/sched/signal.h"
 
@@ -21,8 +29,8 @@ static struct task_struct *thread = NULL;
 
 // bool isCreateSock = false;
 
-/*pid_t get_pid_by_name(const char* process_name) {
-    struct task_struct* tasks;
+pid_t get_pid_by_name(const char *process_name) {
+    struct task_struct *tasks;
     pid_t pid = -1;
 
     read_lock(&tasklist_lock);
@@ -36,7 +44,7 @@ static struct task_struct *thread = NULL;
     read_unlock(&tasklist_lock);
 
     return pid;
-}*/
+}
 
 /*static int server_thread() {
     struct socket *sock, *client_sock;
@@ -59,32 +67,26 @@ static struct task_struct *thread = NULL;
 
 }*/
 
+struct Entity {
+    short obj_id;
+    char camp;
+    int x;
+    int y;
+};
+
+struct GameCorePacket {
+    char PacketLen;
+    struct Entity LocalPlayer;
+} GameCore;
+
 
 static struct task_struct *listen_thread = NULL;
+static struct task_struct *loop_thread = NULL;
 static struct socket *listen_socket = NULL;
 
 bool isCreateSock = false;
 bool isBindServer = false;
 bool isListenSock = false;
-
-char *inet_ntoa(struct in_addr *in) {
-    char *str_ip = NULL;
-    u_int32_t int_ip = 0;
-
-    str_ip = kmalloc(16 * sizeof(char), GFP_KERNEL);
-
-    if (!str_ip)
-        return NULL;
-    else
-        memset(str_ip, 0, 16);
-
-    int_ip = in->s_addr;
-
-    sprintf(str_ip, "%d.%d.%d.%d", (int_ip) & 0xFF, (int_ip >> 8) & 0xFF,
-            (int_ip >> 16) & 0xFF, (int_ip >> 16) & 0xFF);
-
-    return str_ip;
-}
 
 int tcp_server_listen(void *unused) {
 
@@ -139,12 +141,12 @@ int tcp_server_listen(void *unused) {
                             }
 
                             if (client_addr.ss_family == AF_INET) {
-                                struct in_addr *ipv4 = (struct in_addr *) &client_addr;
-                                char *ip = inet_ntoa(ipv4);
-                                if (ip) {
-                                    pr_info("connect client ip: %s\n", ip);
-                                    kfree(ip);
-                                }
+                                struct sockaddr_in *ipv4 = (struct sockaddr_in *) &client_addr;
+                                const uint8_t *ipv4_str = (uint8_t *) &(ipv4->sin_addr);
+                                char ip_str[INET6_ADDRSTRLEN];
+                                snprintf(ip_str, INET_ADDRSTRLEN, "%d.%d.%d.%d", ipv4_str[0], ipv4_str[1], ipv4_str[2],
+                                         ipv4_str[3]);
+                                pr_info("connect client ip: %s\n", ip_str);
 
                                 char *send_buf = NULL;
                                 send_buf = kmalloc(1024, GFP_KERNEL);
@@ -154,21 +156,25 @@ int tcp_server_listen(void *unused) {
                                     sock_release(remote_socket);
                                     continue;
                                 }
-                                struct kvec send_vec;
-                                struct msghdr send_msg;
-                                memset(send_buf, 'a', 1024);
-                                memset(&send_msg, 0, sizeof(send_msg));
-                                memset(&send_vec, 0, sizeof(send_vec));
-                                int send_err = kernel_sendmsg(remote_socket, &send_msg, &send_vec, 1, 1024);
-                                kfree(send_buf);
-                                if (send_err < 0) {
-                                    pr_info("kernel send msg error: %d\n", send_err);
-                                    kernel_sock_shutdown(remote_socket, SHUT_RDWR);
-                                    sock_release(remote_socket);
-                                    continue;
+                                while (true) {
+                                    struct kvec send_vec;
+                                    struct msghdr send_msg;
+                                    memset(send_buf, 'a', 1024);
+                                    memset(&send_msg, 0, sizeof(send_msg));
+                                    memset(&send_vec, 0, sizeof(send_vec));
+                                    send_vec.iov_base = &GameCore;
+                                    send_vec.iov_len = sizeof(GameCore);
+                                    int send_err = kernel_sendmsg(remote_socket, &send_msg, &send_vec, 1,
+                                                                  sizeof(GameCore));
+                                    kfree(send_buf);
+                                    if (send_err < 0) {
+                                        pr_info("kernel send msg error: %d\n", send_err);
+                                        kernel_sock_shutdown(remote_socket, SHUT_RDWR);
+                                        sock_release(remote_socket);
+                                        break;
+                                    }
+                                    msleep(500);
                                 }
-                                kernel_sock_shutdown(remote_socket, SHUT_RDWR);
-                                sock_release(remote_socket);
                             } else {
                                 kernel_sock_shutdown(remote_socket, SHUT_RDWR);
                                 sock_release(remote_socket);
@@ -185,12 +191,105 @@ int tcp_server_listen(void *unused) {
     }
 }
 
+/*
+phys_addr_t translate_linear_address(struct mm_struct* mm, uintptr_t va) {
+
+    pgd_t *pgd;
+    pmd_t *pmd;
+    pte_t *pte;
+    pud_t *pud;
+
+    phys_addr_t page_addr;
+    uintptr_t page_offset;
+
+    pgd = pgd_offset(mm, va);
+    if(pgd_none(*pgd) || pgd_bad(*pgd)) {
+        return 0;
+    }
+    pud = pud_offset(pgd,va);
+    if(pud_none(*pud) || pud_bad(*pud)) {
+        return 0;
+    }
+    pmd = pmd_offset(pud,va);
+    if(pmd_none(*pmd)) {
+        return 0;
+    }
+    pte = pte_offset_kernel(pmd,va);
+    if(pte_none(*pte)) {
+        return 0;
+    }
+    if(!pte_present(*pte)) {
+        return 0;
+    }
+    //页物理地址
+    page_addr = (phys_addr_t)(pte_pfn(*pte) << PAGE_SHIFT);
+    //页内偏移
+    page_offset = va & (PAGE_SIZE-1);
+
+    return page_addr + page_offset;
+}
+
+#ifndef ARCH_HAS_VALID_PHYS_ADDR_RANGE
+static inline int valid_phys_addr_range(phys_addr_t addr, size_t count) {
+    return addr + count <= __pa(high_memory);
+}
+#endif
+
+bool read_proc_mem(
+        pid_t pid,
+        uintptr_t addr,
+        void *buffer,
+        size_t size) {
+
+    struct task_struct *task;
+    struct mm_struct *mm;
+    struct pid *pid_struct;
+    phys_addr_t pa;
+
+    pid_struct = find_get_pid(pid);
+    if (!pid_struct) {
+        return false;
+    }
+    task = get_pid_task(pid_struct, PIDTYPE_PID);
+    if (!task) {
+        return false;
+    }
+    mm = get_task_mm(task);
+    if (!mm) {
+        return false;
+    }
+    mmput(mm);
+    pa = translate_linear_address(mm, addr);
+    if (!pa) {
+        return false;
+    }
+}
+*/
+
+
+int game_loop_callback(void *unused) {
+    while (!kthread_should_stop()) {
+        pid_t tgame = get_pid_by_name("com.tencent.tmgp.sgame");
+        if (tgame != -1) {
+            pr_info("tgame_pid: %d\n", tgame);
+        }
+
+        msleep(3000);
+    }
+}
+
 int tcp_server_start(void) {
 
     listen_thread = kthread_run((void *) tcp_server_listen, NULL, "tcp-server");
     if (IS_ERR(listen_thread)) {
-        pr_info("failed to create server thread\n");
+        pr_info("failed to create listen thread\n");
         return PTR_ERR(listen_thread);
+    }
+
+    loop_thread = kthread_run((void *) game_loop_callback, NULL, "loop");
+    if (IS_ERR(loop_thread)) {
+        pr_info("failed to create loop thread\n");
+        return PTR_ERR(loop_thread);
     }
     return 0;
 }
